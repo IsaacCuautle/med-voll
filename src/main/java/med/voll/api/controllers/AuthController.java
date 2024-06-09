9afd09093c6561e045a1346b8341a7efa457f1db
Luis Isaac Cuautle Delgadillo <cuautle.tron@gmail.com>
2024-06-09 00:03:38 -0600

    Implementando JWT
    - Se creo la clase AuthController para menejar la autenticacion mediante JWT
    
    -Se creo el servicio Authentication para buscar usurios y claves
    
    - Se creo el registro JwtData
    
    - Se añadio la dependencia spring security y java jwt
    
    - Se creo SecurityConfiguration para cambiar la utenticacio a serverless
    
    - Se creo TokenService para manejar la creacion de los tokens
    
    - Se creo el modelo User para mapear la tabla de usuarios
    
    - Se creo el registro UserDataAuth para mapear los campos de la tabla usuarios
    
    - Se creo UserRepository para las consultas a la tabla de usuarios
    
    - Se creo V5__create-table-users.sql para crear la tabla de usuarios

---
+++ src/main/java/med/voll/api/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package med.voll.api.controllers;
+
+import jakarta.validation.Valid;
+import med.voll.api.dto.JwtData;
+import med.voll.api.dto.user.UserAuthData;
+import med.voll.api.models.User;
+import med.voll.api.services.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("login")
+public class AuthController
+{
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity userAuth(@Valid @RequestBody UserAuthData userAuthData)
+    {
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(userAuthData.login(), userAuthData.password());
+        var  authUser = authenticationManager.authenticate(authToken);
+        var jwt = tokenService.tokenGenerate((User) authUser.getPrincipal());
+        return ResponseEntity.ok(new JwtData(jwt));
+    }
+
+}
---
+++ src/main/java/med/voll/api/dto/user/UserAuthData.java
@@ -0,0 +1,8 @@
+package med.voll.api.dto.user;
+
+public record UserAuthData(
+    String login,
+    String password
+)
+{
+}
